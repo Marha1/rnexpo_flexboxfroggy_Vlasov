d0a3a8131166ce6f8fb522291a8295ef7d61d188
Aleksey <oguralex_2006@mail.ru>
2023-09-27 10:46:16 +0300

    njhnj

--- App.tsx
+++ App.tsx
@@ -1,204 +1,281 @@
-import React, {useState} from "react";
-import { Text, SafeAreaView, StyleSheet, Pressable, View} from 'react-native';
+import React, { useState } from "react";
+import {
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    Pressable,
+    View,
+    Image,
+} from "react-native";
 
 type FrogProps = {
-name: string;
-color: string;
+    name: string;
+    color: string;
 };
 
 const Frog = (props: FrogProps) => {
-const frogstylearray = StyleSheet.compose(styles.frog, {backgroundColor : props.color});
-return(
-<SafeAreaView style = {frogstylearray}>
-<Text>
-{props.name}
-</Text>
-
-</SafeAreaView>
-);
+    const frogstylearray = StyleSheet.compose(styles.frog, {
+        backgroundColor: props.color,
+    });
+    return (
+        <SafeAreaView style={frogstylearray}>
+            <Text>{props.name}</Text>
+        </SafeAreaView>
+    );
 };
 
 const Lake = () => {
-const [isButton, SetIsButton] = useState(0);
-let Lakestylearry;
-switch(isButton){
-case 1:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex01); break;
-case 2:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex02); break;
-case 3:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex03); break;
-case 4:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex04); break;
-case 5:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex05); break;
-case 6:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex06); break;
-case 7:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex07); break;
-case 8:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex08); break;
-case 9:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex09); break;
-case 10:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex10); break;
-case 11:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex11); break;
-case 12:Lakestylearry = StyleSheet.compose(styles.lake, styles.ex12); break;
-}
-return(
-<View style={styles.main}>
-<SafeAreaView style={[styles.lake, Lakestylearry]}>
-<Frog name='1' color = 'green'/>
-<Frog name='2' color = 'yellow'/>
-<Frog name='3' color = 'red'/>
-</SafeAreaView>
-<SafeAreaView style={styles.buttons}>
-<Pressable
-onPress = {() => {SetIsButton(1);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 1</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(2);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 2</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(3);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 3</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(4);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 4</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(5);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 5</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(6);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 6</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(7);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 7</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(8);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 8</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(9);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 9</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(10);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 10</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(11);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 11</Text></Pressable>
-<Pressable
-onPress = {() => {SetIsButton(12);}}
-style={styles.button}
-><Text style={styles.textbutton}>Кнопка 12</Text></Pressable>
-</SafeAreaView>
-< SafeAreaView  style={styles.Text11}>
-<Text style={styles.Text1}>Текущая задача : {isButton}</Text>
-</SafeAreaView>
-
-</View>
-);
+    const [isButton, SetIsButton] = useState(0);
+    let Lakestylearry;
+    switch (isButton) {
+        case 1:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex01);
+            break;
+        case 2:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex02);
+            break;
+        case 3:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex03);
+            break;
+        case 4:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex04);
+            break;
+        case 5:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex05);
+            break;
+        case 6:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex06);
+            break;
+        case 7:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex07);
+            break;
+        case 8:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex08);
+            break;
+        case 9:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex09);
+            break;
+        case 10:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex10);
+            break;
+        case 11:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex11);
+            break;
+        case 12:
+            Lakestylearry = StyleSheet.compose(styles.lake, styles.ex12);
+            break;
+    }
+    return (
+        <View style={styles.main}>
+            <SafeAreaView style={[styles.lake, Lakestylearry]}>
+                <Frog name="1" color="green" />
+                <Frog name="2" color="yellow" />
+                <Frog name="3" color="red" />
+            </SafeAreaView>
+            <SafeAreaView style={styles.buttons}>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(1);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 1</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(2);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 2</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(3);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 3</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(4);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 4</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(5);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 5</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(6);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 6</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(7);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 7</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(8);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 8</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(9);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 9</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(10);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 10</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(11);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>№ 11 </Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        SetIsButton(12);
+                    }}
+                    style={styles.button}
+                >
+                    <Text style={styles.textbutton}>Кнопка 12</Text>
+                </Pressable>
+            </SafeAreaView>
+            <SafeAreaView style={styles.Text11}>
+                <Text style={styles.Text1}>Текущая задача : {isButton}</Text>
+            </SafeAreaView>
+        </View>
+    );
 };
 
 export default Lake;
 
-
 const styles = StyleSheet.create({
-main:{
-backgroundColor : 'grey',
-height : '100%',
-width: "100%",
-},
-lake:{
-backgroundColor: 'aquamarine',
-height : '70%',
+    main: {
+        backgroundColor: "grey",
+        height: "100%",
+        width: "100%",
+    },
+    lake: {
+        backgroundColor: "aquamarine",
+        height: "70%",
+    },
+    buttons: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        height: "20%",
+        backgroundColor: "aquamarine",
+        justifyContent: 'space-between',
+    },
+    Text11: {
+        borderRadius: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        height: "10%",
+        width: "100%",
+        backgroundColor: "aquamarine",
+    },
+    button: {
+        borderWidth: 1,
+        backgroundColor: "green",
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center",
 
-},
-buttons:{
-flexDirection:"row",
-flexWrap: "wrap",
-height:'20%',
-backgroundColor: 'aquamarine',
-},
-Text11:{
- borderRadius: 5,
-    alignItems: "center",
-justifyContent:"center",
-flexWrap: "wrap",
-height:'10%',
-width:'100%',
-backgroundColor: 'aquamarine',
-},
-button:{
-borderWidth:1,
-backgroundColor: "green",
-width:"25%",
-borderRadius: 10,
-alignItems: "center",
-justifyContent:"center",
-},
-textbutton:{
-fontWeight: "700"
-},
-frog:{
-height : 50,
-width: 50,
-borderWidth: 1,
-alignItems : "center",
-justifyContent: "center",
-
-},
-ex01:{
-flexDirection: "row",
-justifyContent: "flex-end",
-},
-ex02:{
-flexDirection: "row",
-justifyContent: "center",
-},
-ex03:{
-flexDirection:"row",
-justifyContent: "space-around",
-},
-ex04:{
-flexDirection:"row",
-justifyContent:"space-between",
-},
-ex05:{
-flexDirection:"row",
-alignItems: "flex-end"
-},
-ex06:{
-flexDirection:"row",
-alignItems: "center",
-justifyContent: 'center',
-},
-ex07:{
-flexDirection:"row",
-alignItems: "flex-end",
-justifyContent: 'space-around',
-},
-ex08:{
-flexDirection:"row-reverse",
-},
-ex09:{
-display:'flex',
-flexDirection: "column",
-},
-ex10:{
-display:'flex',
-flexDirection:"row-reverse",
-justifyContent:'flex-end',
-},
-ex11:{
-flexDirection:'column',
-justifyContent:'flex-end',
-},
-ex12:{
-flexDirection:'column-reverse',
-justifyContent:"space-between",
-},
-Text1:{
-color: 'black',
-}
+    },
+    textbutton: {
+        fontWeight: "700",
+    },
+    frog: {
+        height: 50,
+        width: 50,
+        borderWidth: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    ex01: {
+        flexDirection: "row",
+        justifyContent: "flex-end",
+    },
+    ex02: {
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    ex03: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+    },
+    ex04: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    ex05: {
+        flexDirection: "row",
+        alignItems: "flex-end",
+    },
+    ex06: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    ex07: {
+        flexDirection: "row",
+        alignItems: "flex-end",
+        justifyContent: "space-around",
+    },
+    ex08: {
+        flexDirection: "row-reverse",
+    },
+    ex09: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    ex10: {
+        display: "flex",
+        flexDirection: "row-reverse",
+        justifyContent: "flex-end",
+    },
+    ex11: {
+        flexDirection: "column",
+        justifyContent: "flex-end",
+    },
+    ex12: {
+        flexDirection: "column-reverse",
+        justifyContent: "space-between",
+    },
+    Text1: {
+        color: "black",
+    },
 });
